732a27a1c834b54975e515f18a52708c93a86ff3
Ekenayy <ekenayy@gmail.com>
2020-11-24 20:24:49 -0500

    Project completed in 52 mins

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,10 @@
 class Mission < ApplicationRecord
     belongs_to :planet
     belongs_to :scientist
+
+    validates :name, uniqueness: true
+    validates_presence_of :scientist_id
+    validates_presence_of :planet_id
+    validates_presence_of :name
+
 end
